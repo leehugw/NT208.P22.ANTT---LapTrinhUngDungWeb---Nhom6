194f1f7630c11603324af03e5e2a791d5c91e929
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-03-15 21:31:44 +0700

    feat(database): connect to MongoDB and add collections

--- Database/SaveToMongo/saveToMongo.js
+++ Database/SaveToMongo/saveToMongo.js
@@ -5,21 +5,36 @@ const Contact = require('./models/Contact.js');
 const Family = require('./models/Family.js');
 const Address = require('./models/Address.js');
 
+const Score = require('./models/Score.js');
+const Semester = require('./models/Semester.js');
+const Semester_gpa = require('./models/Semester_gpa.js');
+const Student_gpa = require('./models/Student_gpa.js');
+
 const studentData = require('./Json/student.json');
 const identityData = require('./Json/identity.json');
 const contactData = require('./Json/contact.json');
 const familyData = require('./Json/family.json');
 const addressData = require('./Json/address.json');
 
-mongoose.connect('mongodb+srv://myuser:mypassword@cluster.unplw.mongodb.net/DataBase', {
+const scoreData = require('./Json/score.json');
+const semesterData = require('./Json/semester.json');
+const semesterGPAData = require('./Json/semester_gpa.json');
+const studentGPAData = require('./Json/student_gpa.json'); 
+
+mongoose.connect('X', {
 }).then(() => console.log('MongoDB connected'));
 
 const saveData = async () => {
-    await Student.create(studentData);
-    await Identity.create(identityData);
-    await Contact.create(contactData);
-    await Family.create(familyData);
-    await Address.create(addressData);
+    //await Student.create(studentData);
+    //await Identity.create(identityData);
+    //await Contact.create(contactData);
+    //await Family.create(familyData);
+    //await Address.create(addressData);
+
+    await Score.insertMany(scoreData);
+    await Semester.insertMany(semesterData);
+    await Student_gpa.insertMany(studentGPAData);
+    await Semester_gpa.insertMany(semesterGPAData);
 
     console.log('Dữ liệu đã được lưu vào MongoDB!');
     mongoose.connection.close();
