1f458cf2ebc9c45473bfea9658aad56c8d084bf6
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-04-01 19:52:39 +0700

    fix(DB): Fix Student GPA schema

--- Database/SaveToMongo/models/Student_gpa.js
+++ Database/SaveToMongo/models/Student_gpa.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
-const CumulativeGpaSchema = new mongoose.Schema({
-  student_id: { type: String },
-  total_credits_attempted: { type: Number },
-  total_credits_earned: { type: Number },
-  gpa: { type: Number },
-  cumulative_gpa: { type: Number }
-});
+// Schema cho bảng thống kê GPA tích lũy của sinh viên
+const Student_GpaSchema = new mongoose.Schema({
+  student_id: { type: String, ref: 'Student', required: true, unique: true },
+  total_credits_attempted: { type: Number, default: 0, min: 0 },
+  total_credits_earned: { type: Number, default: 0, min: 0 },
+  gpa: { type: Number, default: 0, min: 0, max: 10 },
+  cumulative_gpa: { type: Number, default: 0, min: 0, max: 10 }
+}, {collection: "student_gpa"}, { timestamps: true });
 
-const CumulativeGpa = mongoose.model("CumulativeGpa", CumulativeGpaSchema);
+const Student_gpa = mongoose.model('Student_gpa', Student_gpaSchema);
 
-module.exports = CumulativeGpa;
+module.exports = Student_Gpa;
