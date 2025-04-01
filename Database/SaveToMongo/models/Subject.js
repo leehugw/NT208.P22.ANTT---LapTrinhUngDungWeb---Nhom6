cceb7035abfc35df3fc48b09c7ba194d6b2db29a
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-04-01 20:06:38 +0700

    feat(DB): add Subject Schema

---
+++ Database/SaveToMongo/models/Subject.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const SubjectSchema = new mongoose.Schema({
+  subjects_id: { type: String, required: true, unique: true },
+  subjects_name: { type: String, required: true },
+  subjectsEL_name: { type: String, required: true },
+  faculty_id: { type: String, ref: "Faculty", required: true }, // Liên kết đến Faculty
+  subjects_type: { type: String, required: true },
+  old_id: { type: [String], default: [] },
+  equivalent_id: { type: [String], default: [] },
+  prerequisite_id: { type: [String], default: [] },
+  previous_id: { type: [String], default: [] },
+  theory_credits: { type: Number, required: true, min: 0 },
+  practice_credits: { type: Number, required: true, min: 0 }
+}, {collection:"subjects"}, { timestamps: true });
+
+const Subject = mongoose.model("Subject", SubjectSchema);
+
+module.exports = Subject;
