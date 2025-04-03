1064f04a1846f0dcd73f069b511592db016f1425
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-04-03 09:35:13 +0700

    fix Score schema

--- Database/SaveToMongo/models/Score.js
+++ Database/SaveToMongo/models/Score.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const ScoreSchema = new mongoose.Schema({
-  score_id: { type: String, unique: true, required: true }, // Mã điểm (duy nhất)
-  subject_id: { type: String, required: true, ref: "Subject" }, // Liên kết với bảng môn học
   student_id: { type: String, required: true, ref: "Student" }, // Liên kết với sinh viên
-  semester_id: { type: String, required: true }, // Học kỳ
+  subject_id: { type: String, required: true, ref: "Subject" }, // Liên kết với bảng môn học
+  semester_id: { type: String, required: true, ref: "Semester"}, // Học kỳ
   score_QT: { type: Number, default: null, min: 0, max: 10 }, // Điểm quá trình
   score_GK: { type: Number, default: null, min: 0, max: 10 }, // Điểm giữa kỳ
   score_TH: { type: Number, default: null, min: 0, max: 10 }, // Điểm thực hành
@@ -12,7 +11,7 @@ const ScoreSchema = new mongoose.Schema({
   score_HP: { type: String, required: true }, // Điểm học phần (có thể là "Miễn")
 
   status: { type: String, enum: ["Đậu", "Rớt"], required: true } // Trạng thái
-}, {collection: "scores"}, { timestamps: true }); // Thêm thời gian tạo/cập nhật
+}, {collection: "scores"}); // Thêm thời gian tạo/cập nhật
 
 const Score = mongoose.model("Score", ScoreSchema);
 module.exports = Score;
