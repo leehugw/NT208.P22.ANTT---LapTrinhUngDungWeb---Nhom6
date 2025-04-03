fac5957824c57a27d48fd817e119b32cdac64693
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-04-03 09:14:54 +0700

    fix semester schema

--- Database/SaveToMongo/models/Semester.js
+++ Database/SaveToMongo/models/Semester.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const SemesterSchema = new mongoose.Schema({
-  semester_id: { type: String },
-  semester_name: { type: String },
-  start_year: { type: Number },
-  end_year: { type: Number }
-});
+  semester_id: { type: String, required: true, unique: true, trim: true },
+  semester_name: { type: String, required: true, trim: true },
+  start_year: { type: Number, required: true, min: 1900, max: new Date().getFullYear() + 1 },
+  end_year: { type: Number, required: true, min: 1900, max: new Date().getFullYear() + 1 }
+}, {collection: "semesters"}, { timestamps: true });
 
 const Semester = mongoose.model("Semester", SemesterSchema);
 
