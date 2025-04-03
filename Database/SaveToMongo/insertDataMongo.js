80647f7220ed8edeee5b67636ce18d797fd16c80
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-04-03 17:36:32 +0700

    feat(DB): add function to delete/insert data from/into MongoDB

---
+++ Database/SaveToMongo/insertDataMongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const insertData = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI);
+        console.log("✅ Đã kết nối MongoDB");
+
+        const db = mongoose.connection.db; // Truy cập database
+        const collection = db.collection("scores"); // Truy cập collection
+
+        // Đọc file JSON
+        const filePath = "Json\\score.json";
+        const jsonData = fs.readFileSync(filePath, 'utf-8');
+        const data = JSON.parse(jsonData); // Chuyển JSON thành object
+
+        // Thêm dữ liệu vào MongoDB
+        const result = await collection.insertMany(data);
+        console.log(`✅ Đã thêm ${result.insertedCount} tài liệu vào MongoDB`);
+
+    } catch (error) {
+            console.error("Lỗi khi chèn dữ liệu:", error);
+    } finally {
+        mongoose.connection.close();
+    }
+};
+
+insertData();
