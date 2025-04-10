5bb19571d24237d7d9fd86aab508efecbc0753c3
Hà Ngọc Ái Trinh <trinhha02022005@gmail.com>
2025-04-10 15:26:04 +0700

    Feedback

--- BackEnd/Routes/admin.js
+++ BackEnd/Routes/admin.js
@@ -9,4 +9,14 @@ router.put('all/academicstatistic/', StudentGPAUpdateController.updateAllGPAs);
 // Cập nhật GPA cho một sinh viên
 router.put(':student_id/academicstatistic/', StudentGPAUpdateController.updateStudentGPA);
 
+// Xem danh sách phản hồi
+const controller = require('../controllers/adminFeedbackController');
+router.get('/admin/feedbacks', controller.getAllFeedbacks);
+
+// Lấy thông tin phản hồi
+const feedbackController = require('../controllers/feedbackController');
+router.post('/feedback', checkAdminAuth, feedbackController.createFeedback);
+router.get('/feedbacks', checkAdminAuth, feedbackController.getAllFeedbacks);
+
+
 module.exports = router;
---
+++ BackEnd/Services/admin/adminFeedbackService.js
@@ -0,0 +1,7 @@
+const { getDB } = require('../db');
+
+exports.getAllFeedbacks = async () => {
+  const db = getDB();
+  const feedbacksCollection = db.collection('feedbacks');
+  return await feedbacksCollection.find({}).toArray();
+};
