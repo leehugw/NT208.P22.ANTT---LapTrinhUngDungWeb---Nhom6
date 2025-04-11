7aefa6db0d976d085a3c84c456412dcf3c9efab0
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-04-11 08:29:54 +0700

    chore(BE): remove unused API files

--- BackEnd/Services/student/StudentGPAViewService.js
+++
@@ -1,8 +0,0 @@
-// services/studentService.js
-const Student_gpa = require('../../../Database/SaveToMongo/models/Student_gpa');
-
-async function getStudentGPAData(student_id) {
-  return Student_gpa.findOne({ student_id: student_id });
-}
-
-module.exports = { getStudentGPAData };
