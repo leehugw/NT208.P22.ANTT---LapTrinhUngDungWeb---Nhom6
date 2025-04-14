802e34a66b4b78d34d23cbfeaa1149fd1e919470
Hà Ngọc Ái Trinh <trinhha02022005@gmail.com>
2025-04-14 23:42:18 +0700

    UI_ChatBot(Update)

--- FrontEnd/UI_ChatBot/UI_ChatBot.js
+++ FrontEnd/UI_ChatBot/UI_ChatBot.js
@@ -1,4 +1,18 @@
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  var menu = document.getElementById('mobile-menu');
+  menu.style.display = 'block';
+  setTimeout(function () {
+      menu.classList.add('open');
+  }, 10);
+});
 
+document.getElementById('menu-close').addEventListener('click', function () {
+  var menu = document.getElementById('mobile-menu');
+  menu.classList.remove('open');
+  setTimeout(function () {
+      menu.style.display = 'none';
+  }, 300);
+});
 // Gửi tin nhắn khi người dùng gửi tin nhắn
 function sendMessage() {
   const inputField = document.getElementById("user-input");
